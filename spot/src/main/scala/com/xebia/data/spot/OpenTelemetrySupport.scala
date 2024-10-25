f02b9d8d1632bec98f04b09d3360bfbfdafd4547
Barend Garvelink <159024183+barend-xebia@users.noreply.github.com>
2024-10-25 13:52:47 +0200

    add startup logging

--- spot/src/main/scala/com/xebia/data/spot/OpenTelemetrySupport.scala
+++ spot/src/main/scala/com/xebia/data/spot/OpenTelemetrySupport.scala
@@ -2,6 +2,7 @@ package com.xebia.data.spot
 
 import io.opentelemetry.api.OpenTelemetry
 import io.opentelemetry.api.trace.Tracer
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
  * Grants access to an OpenTelemetry instance.
@@ -11,14 +12,24 @@ import io.opentelemetry.api.trace.Tracer
  * attempts to load the class indicated by that value.
  */
 trait OpenTelemetrySupport {
+  import OpenTelemetrySupport.logger
+
   def spotConfig: Map[String, String]
 
   val openTelemetry: OpenTelemetry = {
     val provFQCN = spotConfig.getOrElse(configPrefixed("sdkProvider"), DEFAULT_PROVIDER_FQCN)
+    logger.info(s"Using OpenTelemetrySdkProvider FQCN: '$provFQCN'")
     val provClass = java.lang.Class.forName(provFQCN)
     val provider = provClass.getDeclaredConstructor().newInstance().asInstanceOf[OpenTelemetrySdkProvider]
-    provider.get(spotConfig)
+    val otel = provider.get(spotConfig)
+    logger.info(s"OpenTelemetry SDK found: $otel")
+    otel
   }
 
   lazy val tracer: Tracer = openTelemetry.getTracer("spot", version())
+}
+
+object OpenTelemetrySupport {
+  @transient
+  protected lazy val logger: Logger = LoggerFactory.getLogger(classOf[OpenTelemetrySupport])
 }
