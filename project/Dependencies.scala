bea18547ac5cf681b2a2de4d8388332cd3cee6c4
Barend Garvelink <159024183+barend-xebia@users.noreply.github.com>
2024-11-15 10:57:55 +0100

    update OpenTelemetry to 1.44.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private[this] val openTelemetryVersion = "1.43.0"
+  private[this] val openTelemetryVersion = "1.44.1"
 
   val `opentelemetry-api` = "io.opentelemetry" % "opentelemetry-api" % openTelemetryVersion
   val `opentelemetry-sdk` = "io.opentelemetry" % "opentelemetry-sdk" % openTelemetryVersion
